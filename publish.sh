3f31274f72c278f7013ea35a3d6ae4598cfc0847
Erik Ylvisaker <eylvisaker@gmail.com>
2018-10-15 20:40:25 -0700

    Make sure to publish all files.

--- publish.sh
+++ publish.sh
@@ -9,4 +9,4 @@ dest=$FTP_DEST/SmokedGB
 echo "Uploading to ftp://$FTP_REMOTE_HOST/$dest"
 echo "Logging in as $FTP_USERNAME"
 
-ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $FTP_DEST $1/*Windows*.zip
+ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $FTP_DEST $1/*

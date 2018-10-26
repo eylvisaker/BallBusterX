c43a7a393d821f7ba5a44121f728017054ff39e8
Erik Ylvisaker <eylvisaker@gmail.com>
2018-10-26 07:13:40 -0700

    Update build scripts

--- publish.sh
+++ publish.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+source ./project-vars.sh
+
 sourceDir=$1
 remoteHost=$FTP_REMOTE_HOST
 username=$FTP_USERNAME
 password=$FTP_PASSWORD
-dest=$FTP_DEST/SmokedGB
 
-echo "Uploading to ftp://$FTP_REMOTE_HOST/$dest"
+echo "Uploading to ftp://$FTP_REMOTE_HOST/$ProjectFtpDest/$FTP_DEST"
 echo "Logging in as $FTP_USERNAME"
 
-ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $FTP_DEST $1/*
+ftp-upload -h $FTP_REMOTE_HOST -u $FTP_USERNAME --password $FTP_PASSWORD -d $ProjectFtpDest/$FTP_DEST $1/*
+
